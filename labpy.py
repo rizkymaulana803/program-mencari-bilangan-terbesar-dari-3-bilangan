11acfa063cb86fd11e86fb8d15989df50b5454e7
rizkymaulana803 <46336552+rizkymaulana803@users.noreply.github.com>
2019-02-12 22:08:02 +0700

    Add files via upload

---
+++ labpy.py
@@ -0,0 +1,13 @@
+print("program menentukan bilangan terbesar dari 3 bilangan")
+a=int(input ("masukan nilai ke-1 = "))
+b=int(input ("masukan nilai ke-2 = "))
+c=int(input ("masukan nilai ke-3 = "))
+
+print("cari bilangan ",a,b,c)
+
+if a > b and a> c:
+    print(a, "adalah nilai terbesar")
+elif b > a and b > c:
+    print(b, "adalah nilai terbesar")
+else:
+    print(c, "adalah nilai terbesar")
